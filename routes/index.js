c4d94c6097ab98e1057cc68d366e5f1f5d5d4d6f
HanGangHee <tkdlaos21@ajou.ac.kr>
2018-09-03 13:22:42 +0900

    route 설정

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
+const auth = require('./auth')
+const search = require('./search')
+const user = require('./user')
+const wiki = require('./wiki')
+
+
 router.get('/', (req, res, next) => {
     res.send("GET / NOW WORKING")
 })
+router.use('/auth', auth)
+router.use('/search', search)
+router.use('/user', user)
+router.use('/wiki', wiki)
+
+
 
 module.exports = router
