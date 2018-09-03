650170ed84c07b7598f5a769259b8de1f6eff691
HanGangHee <tkdlaos21@ajou.ac.kr>
2018-09-03 13:43:33 +0900

    /wiki route 설정

---
+++ routes/wiki/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+const { Wiki, User } = require('../../models')
+const router= express.Router()
+
+router.get('/wiki', async (req, res, next) => {
+    try {
+        const wikis = await Wiki.findAll({
+            include : [{ model : User }],
+            attributes : ['title', 'likes', 'dislikes'],
+            where : { parentId : 0 },
+            order : ['likes', 'DESC'],
+            limit : 10,
+        })
+        res.json({ data : wikis })
+    } catch (error) {
+        console.error(error)
+        next(error)
+    }
+})
+
+router.get('/wiki/:num', async (req, res, next) => {
+    try {
+        const num = req.params.num
+        const wiki = await Wiki.find({ where : { id:num } })
+        res.json({ data : wiki })
+    } catch(error){
+        console.error(error)
+        next(error)
+    }
+})
+
+module.exports = router
