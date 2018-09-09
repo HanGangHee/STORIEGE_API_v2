7423b44aede3e954d315e090c91dde6192edd663
HanGangHee <tkdlaos21@ajou.ac.kr>
2018-09-09 21:39:28 +0900

    model 추가

--- models/index.js
+++ models/index.js
@@ -10,13 +10,22 @@ db.User = require('./user')(sequelize, Sequelize)
 db.Hashtag = require('./Hashtag')(sequelize, Sequelize)
 db.Wiki = require('./wiki')(sequelize, Sequelize)
 
-db.User.hasMany(db.Wiki)
-db.Wiki.belongsTo(db.User)
+db.User.hasMany(db.Wiki, { foreignKey : 'userId', sourceKey : 'id' })
+db.Wiki.belongsTo(db.User, { foreignKey : 'userId', targetKey : 'id' })
 
 db.Hashtag.belongsToMany(db.Wiki, { through : 'WikiHashtag' })
 db.Wiki.belongsToMany(db.Hashtag, { through : 'WikiHashtag' })
 
+
+db.WikiHashtag = sequelize.define("WikiHashtag", {},{
+    freezeTableName : true,
+    timestamps : true })
+
+db.WikiHashtag.belongsTo(db.Wiki)
+db.WikiHashtag.belongsTo(db.Hashtag)
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
 
 module.exports = db;
