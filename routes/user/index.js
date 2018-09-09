12b22eddaf43e5b46a069b0860a9f82f80ac867a
HanGangHee <tkdlaos21@ajou.ac.kr>
2018-09-09 21:44:53 +0900

    delete 수정

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 
-const { User, Wiki } = require('../../models')
+const { User, Wiki, WikiHashtag } = require('../../models')
 const { isLoggedIn } = require('../middleware')
 const router = express.Router()
 
 
 router.get('/', isLoggedIn, async (req, res, next) => {
     try {
-        const userId = req.user.id
+        const userId = req.user
         const exUser = await User.find( { where : { id : userId } })
         res.json(exUser)
     } catch (error){
@@ -20,11 +20,11 @@ router.get('/', isLoggedIn, async (req, res, next) => {
 
 router.put('/:id', isLoggedIn, async (req, res, next) => {
     try {
-        if(req.params.id != req.user.id){
+        if(req.params.id != req.user){
             res.json({ message : "권한이 없습니다. " })
             return
         }
-        const userId = req.user.id
+        const userId = req.user
         const { nick, age, sex, thema } = req.body
         await User.update({ nick, age, sex, thema }, { where : { id : userId } } )
         res.json( { success : true } )
@@ -37,20 +37,19 @@ router.put('/:id', isLoggedIn, async (req, res, next) => {
 
 router.delete('/:id', isLoggedIn, async (req, res, next) => {
     try {
-        if(req.params.id != req.user.id){
+        if(req.params.id != req.user){
             res.json({ message : "권한이 없습니다. " })
             return
         }
-        const deleteUser = await User.destroy( { where : { id : req.user.id } })
-        const deleteWiki = await deleteUser.removeWikis()
-        await deleteWiki.removeHashtags()
+        await User.destroy( { where : { id : req.user } })
+        const deleteWikis = await Wiki.destroy( { where : { userId : req.user }} )
+        await WikiHashtag.destroy( { where : { wikiId : deleteWikis } })
         res.json( { success : true } )
     } catch (error){
         console.error(error)
         next(error)
     }
 })
-
 
 
 module.exports = router
